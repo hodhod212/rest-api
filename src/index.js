389c5de31247fa085afa4f5e31989909127e44de
Ali Ezadkhaha <hodhod212@yahoo.com>
2019-11-19 14:11:00 +0100

    skapad moduls

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+let express = require('express')
+let app = express()
+let personRoute = require('./routes/person');
+let path = require('path');
+app.use((req,res,next)=>{
+    console.log(`${new Date().toString()}=>${req.originalUrl}`);
+    next();
+})
+app.use(personRoute);
+app.use(express.static('public'));
+app.use((req,res,next)=>{
+    res.status(404).send('Server could not find the requested file.');
+    next();
+});
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.sendFile(path.join(__dirname,'../public/500.html'));
+})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>console.info(`Server has started on ${PORT}`));
